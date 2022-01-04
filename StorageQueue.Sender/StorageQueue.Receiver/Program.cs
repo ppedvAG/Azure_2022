1baf0c36ea6b98eb56fc1445eb25820ffe62b9b9
André <AndreR@outlook.de>
2022-01-04 14:10:35 +0100

    Storage Queue Sender und Receiver

---
+++ StorageQueue.Sender/StorageQueue.Receiver/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Storage.Queues;
+
+Console.WriteLine("Hello RECEIVER");
+
+var conString = "DefaultEndpointsProtocol=https;AccountName=mykursstorage;AccountKey=at1dzBlbOCqq0PCsyH8Z5CAZ8o2pYEm8KUdUV8cQY9ndteySbW5+6KkZ3nFTeb4UNXPEWQvM4eiSV2D+rE3R6Q==;EndpointSuffix=core.windows.net";
+
+
+var client = new QueueClient(conString, "hallowelt");
+
+while (true)
+{
+    var msg = client.ReceiveMessage();
+    if (msg.Value != null)
+    {
+        Console.WriteLine($"{msg.Value.Body} [{msg.Value.MessageId}]");
+        client.DeleteMessage(msg.Value.MessageId, msg.Value.PopReceipt);
+    }
+    //Thread.Sleep(1000);
+}
+
+
+Console.WriteLine("Ende");
+Console.ReadLine();
---
+++ StorageQueue.Sender/StorageQueue.Sender/Program.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Storage.Queues;
+
+Console.WriteLine("Hello SENDER!");
+
+var conString = "DefaultEndpointsProtocol=https;AccountName=mykursstorage;AccountKey=at1dzBlbOCqq0PCsyH8Z5CAZ8o2pYEm8KUdUV8cQY9ndteySbW5+6KkZ3nFTeb4UNXPEWQvM4eiSV2D+rE3R6Q==;EndpointSuffix=core.windows.net";
+
+var client = new QueueClient(conString, "hallowelt");
+
+while (true)
+{
+    client.SendMessage($"Hallo {DateTime.Now}");
+
+    Console.WriteLine("Press any key for next msg");
+    Console.ReadKey();
+}
+
+
+Console.WriteLine("Ende");
+Console.ReadLine();
