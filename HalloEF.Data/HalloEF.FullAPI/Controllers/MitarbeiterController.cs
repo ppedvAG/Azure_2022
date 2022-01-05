ce08801e097af4239ff765c5c660dd7e8f214a7f
André <AndreR@outlook.de>
2022-01-05 10:15:21 +0100

    Jetzt mit WEB API

---
+++ HalloEF.Data/HalloEF.FullAPI/Controllers/MitarbeiterController.cs
@@ -0,0 +1,55 @@
+﻿using HalloEF.Data;
+using HalloEF.Data.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HalloEF.FullAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MitarbeiterController : ControllerBase
+    {
+        EfContext _context = new EfContext();
+
+        // GET: api/<MitarbeiterController>
+        [HttpGet]
+        public IEnumerable<Mitarbeiter> Get()
+        {
+            return _context.Mitarbeiter.ToList();
+        }
+
+        // GET api/<MitarbeiterController>/5
+        [HttpGet("{id}")]
+        public Mitarbeiter Get(int id)
+        {
+            return _context.Mitarbeiter.Find(id);
+        }
+
+        // POST api/<MitarbeiterController>
+        [HttpPost]
+        public void Post([FromBody] Mitarbeiter value)
+        {
+            _context.Mitarbeiter.Add(value);
+            _context.SaveChanges();
+        }
+
+        // PUT api/<MitarbeiterController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Mitarbeiter value)
+        {
+            _context.Mitarbeiter.Update(value);
+            _context.SaveChanges();
+        }
+
+        // DELETE api/<MitarbeiterController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _context.Mitarbeiter.Remove(Get(id));
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ HalloEF.Data/HalloEF.FullAPI/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddControllers(options =>
+{
+    options.OutputFormatters.RemoveType<SystemTextJsonOutputFormatter>();
+    options.OutputFormatters.Add(new SystemTextJsonOutputFormatter(new JsonSerializerOptions(JsonSerializerDefaults.Web) { ReferenceHandler = ReferenceHandler.Preserve }));
+});
+    
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
